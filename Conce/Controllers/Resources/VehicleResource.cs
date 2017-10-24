05c8ea9d4c3954eb30a82fcc3792960873c2d20d
Giovani Harada <giovani@motioncomunicacao.com.br>
2017-10-24 16:10:14 -0200

    Api

--- Conce/Controllers/Resources/VehicleResource.cs
+++ Conce/Controllers/Resources/VehicleResource.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Conce.Models;
+using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.ComponentModel.DataAnnotations;
@@ -10,14 +11,16 @@ namespace Conce.Controllers.Resources
     public class VehicleResource
     {
         public int Id { get; set; }
-        public int ModelId { get; set; }
+        public KeyValuePairResource Model { get; set; }
+        public KeyValuePairResource Make { get; set; }
         public bool IsRegistered { get; set; }
         public ContactResource Contact { get; set; }
-        public ICollection<int> Features { get; set; }
+        public DateTime LastUpdate { get; set; }
+        public ICollection<KeyValuePairResource> Features { get; set; }
 
         public VehicleResource()
         {
-            Features = new Collection<int>();
+            Features = new Collection<KeyValuePairResource>();
         }
     }
 }
--- Conce/Controllers/VehiclesController.cs
+++ Conce/Controllers/VehiclesController.cs
@@ -7,6 +7,7 @@ using Conce.Models;
 using Conce.Controllers.Resources;
 using AutoMapper;
 using Conce.Data;
+using Microsoft.EntityFrameworkCore;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -23,31 +24,65 @@ namespace Conce.Controllers
             this.ctx = ctx;
             this.mapper = mapper;
         }
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetVehicle(int id)
+        {
+            var vehicle = await ctx.Vehicles
+                .Include(v => v.Features)
+                    .ThenInclude(vf => vf.Feature)
+                .Include(v => v.Model)
+                    .ThenInclude(m => m.Make)
+                .Where(v => v.Id == id).SingleOrDefaultAsync();
+
+            if (vehicle == null)
+                return NotFound();
+
+            var vehicleResoure = mapper.Map<Vehicle, VehicleResource>(vehicle);
+            return Ok(vehicleResoure);
+        }
+
         [HttpPost]
-        public async Task<IActionResult> Post([FromBody] VehicleResource vehicleResource)
+        public async Task<IActionResult> Post([FromBody] SaveVehicleResource vehicleResource)
         {
             if (!ModelState.IsValid)
                 return BadRequest(ModelState);
 
-            var vehicle = mapper.Map<VehicleResource, Vehicle>(vehicleResource);
+            var vehicle = mapper.Map<SaveVehicleResource, Vehicle>(vehicleResource);
             vehicle.LastUpdate = DateTime.Now;
             ctx.Vehicles.Add(vehicle);
             await ctx.SaveChangesAsync();
-            var result = mapper.Map<Vehicle, VehicleResource>(vehicle);
+            var result = mapper.Map<Vehicle, SaveVehicleResource>(vehicle);
             return Ok(result);
         }
         [HttpPut("{id}")]
-        public async Task<IActionResult> UpdateVehicle(int id, [FromBody] VehicleResource vehicleResource)
+        public async Task<IActionResult> UpdateVehicle(int id, [FromBody] SaveVehicleResource vehicleResource)
         {
             if (!ModelState.IsValid)
                 return BadRequest(ModelState);
 
-            var vehicle = await ctx.Vehicles.FindAsync(id);
-            mapper.Map<VehicleResource, Vehicle>(vehicleResource, vehicle);
+            var vehicle = await ctx.Vehicles.Include(v => v.Features).SingleOrDefaultAsync(v => v.Id == id);
+
+            if (vehicle == null)
+                return NotFound();
+
+            mapper.Map<SaveVehicleResource, Vehicle>(vehicleResource, vehicle);
             vehicle.LastUpdate = DateTime.Now;
             await ctx.SaveChangesAsync();
-            var result = mapper.Map<Vehicle, VehicleResource>(vehicle);
+            var result = mapper.Map<Vehicle, SaveVehicleResource>(vehicle);
             return Ok(result);
+        }
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteVehicle(int id)
+        {
+            var vehicle = await ctx.Vehicles.FindAsync(id);
+
+            if (vehicle == null)
+                return NotFound();
+
+            ctx.Remove(vehicle);
+            await ctx.SaveChangesAsync();
+
+            return Ok(id);
         }
     }
 }
