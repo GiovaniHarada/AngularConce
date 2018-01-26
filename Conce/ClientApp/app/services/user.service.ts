ef6ed189c10e57c8b3096d1489e6ec21cee00344
Giovani Harada <vodkahd23@gmail.com>
2018-01-26 16:02:47 -0200

    Angular Register wip

--- Conce/ClientApp/app/services/user.service.ts
+++ Conce/ClientApp/app/services/user.service.ts
@@ -6,10 +6,11 @@ import { ConfigService } from '../utils/config.service';
 
 import { Observable } from 'rxjs/Rx';
 import { BehaviorSubject } from 'rxjs/Rx';
+import { BaseService } from './base.service';
 
 
 @Injectable()
-export class UserService {
+export class UserService extends BaseService {
     baseUrl: string = '';
 
     private _authNavStatusSource = new BehaviorSubject<boolean>(false);
@@ -19,19 +20,49 @@ export class UserService {
     private loggedIn = false;
 
     constructor(private http: Http, private configService: ConfigService) {
+        super();
         this.loggedIn = !!localStorage.getItem('auth_token');
 
         this._authNavStatusSource.next(this.loggedIn);
         this.baseUrl = configService.getApiURI();
     }
 
-    register(email: string, password: string, firstName: string, lastName: string): Observable<UserRegistration> {
-        let body = JSON.stringify({ email, password, firstName, lastName });
+    register(email: string, password: string, firstName: string, lastName: string, location: string): Observable<UserRegistration> {
+        let body = JSON.stringify({ email, password, firstName, lastName, location });
         let headers = new Headers({ 'Content-Type': 'application/json' });
         let options = new RequestOptions({ headers: headers });
 
         return this.http.post(this.baseUrl + "/accounts", body, options)
-            .map(res => true);
+            .map(res => true)
+            .catch(this.handleError);
+    } 
 
+    login(userName, password) {
+        let headers = new Headers();
+        headers.append('Content-Type', 'application/json');
+
+        return this.http
+            .post(
+            this.baseUrl + '/auth/login',
+            JSON.stringify({ userName, password }), { headers }
+            )
+            .map(res => res.json())
+            .map(res => {
+                localStorage.setItem('auth_token', res.auth_token);
+                this.loggedIn = true;
+                this._authNavStatusSource.next(true);
+                return true;
+            })
+            .catch(this.handleError);
+    }
+
+    logout() {
+        localStorage.removeItem('auth_token');
+        this.loggedIn = false;
+        this._authNavStatusSource.next(false);
+    }
+
+    isLoggedIn() {
+        return this.loggedIn;
     }
 }
